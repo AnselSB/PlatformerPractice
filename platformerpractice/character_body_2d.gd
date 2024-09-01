e0a5d03848612502d3657be0bb708560de9b995f
SandersWifeBernie <117328851+SandersWifeBernie@users.noreply.github.com>
2024-09-01 13:45:38 -0600

    Implement fast fall and sudden stop

--- platformerpractice/character_body_2d.gd
+++ platformerpractice/character_body_2d.gd
@@ -2,13 +2,16 @@ extends CharacterBody2D
 
 
 @export var  SPEED = 300.0
-@export var JUMP_VELOCITY = -400.0
+@export var JUMP_VELOCITY = -600.0
 @export var DECEL = 290
-
+@export var fastFallRate = 1.1
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity= velocity + get_gravity() * delta
+		if Input.is_action_pressed("down"):
+			velocity = velocity  + get_gravity() * delta * fastFallRate
+		else:
+			velocity= velocity + get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("up") and is_on_floor():
@@ -29,6 +32,6 @@ func _physics_process(delta):
 		velocity.x = velocity.x + DECEL * delta * -1
 		if velocity.x < 0:
 			velocity.x = 0
-	if Input.is_action_pressed("down"):
+	if Input.is_action_pressed("down") and is_on_floor():
 		velocity.x = 0
 	move_and_slide()
